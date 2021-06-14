29704628a61240f5b0e21af10d0feffeb43e415e
Colin386 <ngngaitengcolin@outlook.com>
2021-06-14 22:14:10 +0800

    added formatting changes

--- project1/App.js
+++ project1/App.js
@@ -10,36 +10,38 @@ export default function App() {
       <StatusBar style="auto" />
 
       <Form>
-  <Form.Row className="align-items-center">
-    <Col xs="auto" className="my-1">
-      <Form.Label className="mr-sm-2" htmlFor="inlineFormCustomSelect" srOnly>
-        Preference
-      </Form.Label>
-      <Form.Control
-        as="select"
-        className="mr-sm-2"
-        id="inlineFormCustomSelect"
-        custom
-      >
-        <option value="0">Choose...</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-      </Form.Control>
-    </Col>
-    <Col xs="auto" className="my-1">
-      <Form.Check
-        type="checkbox"
-        id="customControlAutosizing"
-        label="Remember my preference"
-        custom
-      />
-    </Col>
-    <Col xs="auto" className="my-1">
-      <Button type="submit">Submit</Button>
-    </Col>
-  </Form.Row>
-</Form>
+        <Form.Row className="align-items-center">
+        <Col xs="auto" className="my-1">
+          <Form.Label className="mr-sm-2" htmlFor="inlineFormCustomSelect" srOnly>
+            Preference
+          </Form.Label>
+          <Form.Control
+            as="select"
+            className="mr-sm-2"
+            id="inlineFormCustomSelect"
+            custom
+          >
+            <option value="0">Choose...</option>
+            <option value="1">Ion Orchard</option>
+            <option value="2">Shaw House</option>
+            <option value="3">Lucky Plaza</option>
+            <option value="4">Wisma Atria</option>
+            <option value="5">Ngee Ann City</option>
+          </Form.Control>
+        </Col>
+        <Col xs="auto" className="my-1">
+          <Form.Check
+            type="checkbox"
+            id="customControlAutosizing"
+            label="Remember my preference"
+            custom
+          />
+        </Col>
+        <Col xs="auto" className="my-1">
+          <Button type="submit">Submit</Button>
+        </Col>
+      </Form.Row>
+    </Form>
     </View>
   );
 }
