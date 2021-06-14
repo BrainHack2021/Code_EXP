8de14d2ff5b40f75204067e4fac0464d2c626b13
Colin386 <ngngaitengcolin@outlook.com>
2021-06-14 21:59:44 +0800

    added very basic dropdown menu

--- project1/App.js
+++ project1/App.js
@@ -1,13 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Forms } from 'react-bootstrap';
+import { Form, Col, Button } from 'react-bootstrap';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Form>
+  <Form.Row className="align-items-center">
+    <Col xs="auto" className="my-1">
+      <Form.Label className="mr-sm-2" htmlFor="inlineFormCustomSelect" srOnly>
+        Preference
+      </Form.Label>
+      <Form.Control
+        as="select"
+        className="mr-sm-2"
+        id="inlineFormCustomSelect"
+        custom
+      >
+        <option value="0">Choose...</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </Form.Control>
+    </Col>
+    <Col xs="auto" className="my-1">
+      <Form.Check
+        type="checkbox"
+        id="customControlAutosizing"
+        label="Remember my preference"
+        custom
+      />
+    </Col>
+    <Col xs="auto" className="my-1">
+      <Button type="submit">Submit</Button>
+    </Col>
+  </Form.Row>
+</Form>
     </View>
   );
 }
