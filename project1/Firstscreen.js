6cb1a1f7339afd58cf28c1761d8fe7fedccdaa36
homingjun <homingjun9@gmail.com>
2021-06-15 17:51:14 +0800

    Connect two pickers

--- project1/Firstscreen.js
+++ project1/Firstscreen.js
@@ -1,36 +1,88 @@
 import React, { useState } from "react";
-import { Button, View, Picker, StyleSheet } from "react-native";
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Button, View, Picker, StyleSheet, Switch } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { propTypes } from "react-bootstrap/esm/Image";
 
 export default function chooser({ navigation: { navigate } }) {
-  const [selectedValue, setSelectedValue] = useState("java");
-
+  const [locationId, setSelectedId] = useState("1");
+  const [locationOutlet, setSelectedOutlet] = useState(0);
 
   return (
     <View>
-      <Picker
-        selectedValue={selectedValue}
-        style={{ height: 50, width: 150 }}
-        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-      >
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
-      </Picker>
-      
-      <Button
-        title="Go to Profile"
-        onPress={() => navigate('Profile', { screen: 'Headcount'})} //additional of a button to navigate elsewhere
-      />
+      <View style={styles.dropdownBox}>
+        <Picker
+          selectedValue={locationId}
+          style={{ height: 50, width: 150 }}
+          onValueChange={(itemValue) => setSelectedId(itemValue)}
+        >
+          <Picker.Item label="Ion" value="1" />
+          <Picker.Item label="Wisma" value="2" />
+          <Picker.Item label="Taka" value="3" />
+        </Picker>
+      </View>
+      <View style={styles.dropdownBox}>
+        {(() => {
+          switch (locationId) {
+            case "1":
+              return (
+                <Picker
+                  selectedValue={locationOutlet}
+                  style={{ height: 50, width: 150 }}
+                  onValueChange={(itemValue) => setSelectedOutlet(itemValue)}
+                >
+                  <Picker.Item label="Fuck" value="f" />;
+                  <Picker.Item label="This" value="t" />;
+                  <Picker.Item label="Nonsense" value="s" />;
+                </Picker>
+              );
+
+            case "2":
+              return (
+                <Picker
+                  selectedValue={locationOutlet}
+                  style={{ height: 50, width: 150 }}
+                  onValueChange={(itemValue) => setSelectedOutlet(itemValue)}
+                >
+                  <Picker.Item label="Take me 2 hrs knn wtf" value="x" />;
+                </Picker>
+              );
+
+            case "3":
+              return (
+                <Picker
+                  selectedValue={locationOutlet}
+                  style={{ height: 50, width: 150 }}
+                  onValueChange={(itemValue) => setSelectedOutlet(itemValue)}
+                >
+                  <Picker.Item label="Jesussss" value="fml" />;
+                </Picker>
+              );
+          }
+        })()}
+      </View>
+      <View style={styles.profileBox}>
+        <Button
+          color="orange"
+          title="Go to Profile"
+          onPress={() => navigate("Profile", { screen: "Headcount" })} //additional of a button to navigate elsewhere
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  dropdownBox: {
     flex: 1,
-    paddingTop: 40,
-    alignItems: "center"
-  }
+    margin: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileBox: {
+    flex: 1,
+    paddingTop: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
