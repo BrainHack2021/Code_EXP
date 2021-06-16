1bfc4bd3c3ee2faa2531ae3033cf336002ba18b1
homingjun <homingjun9@gmail.com>
2021-06-17 00:30:07 +0800

    update design

--- project1/App.js
+++ project1/App.js
@@ -1,94 +1,98 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { DataTable } from 'react-native-paper';
+import React, { useState } from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { DataTable } from "react-native-paper";
 
 // npm i @react-navigation/bottom-tabs react-native-elements react-native-screens @react-navigation/native
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from 'react-native-elements';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import  chooser from './Firstscreen';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import chooser from "./Firstscreen";
+
+const image = {
+  uri: "https://static.wixstatic.com/media/4cbe8d_f1ed2800a49649848102c68fc5a66e53~mv2.gif",
+};
 
 var mallDict = {
   0: "No Mall Selected",
   1: "Ion Orchard",
   2: "Wisma Atria",
-  3: "Ngee Ann City"
-}
+  3: "Ngee Ann City",
+};
 
 var storeDict = {
-  'a':'4 fingers crispy chicken',
-  'b':'7-Eleven',
-  'c':'Best Denki',
-  'd':'Cartier',
-  'e':'Challenger',
-  'f':'Chanel',
-  'g':'Cotton On Body',
-  'h':'Din Tai Fung',
-  'i':'Famous Amos',
-  'j':'Fila',
-  'k':'Forever New',
-  'l':'Garmin',
-  'm':'Imperial Treausre Fine Shanghai Cuisine',
-  'n':'Kanshoku Ramen Bar',
-  'o':'Lacoste',
-  'p':'Louis Vuitton',
-  'q':'Mango',
-  'r':'Omega',
-  's':'Rolex',
-  't':'Seiko',
-  'u':'Superdry',
-  'v':'Tailored Fashions',
-  'w':'Tampopo Ramen',
-  'x':'Typo',
-  'y':'Yanmi Yogurt',
-  0:'No selection'
-}
+  a: "4 fingers crispy chicken",
+  b: "7-Eleven",
+  c: "Best Denki",
+  d: "Cartier",
+  e: "Challenger",
+  f: "Chanel",
+  g: "Cotton On Body",
+  h: "Din Tai Fung",
+  i: "Famous Amos",
+  j: "Fila",
+  k: "Forever New",
+  l: "Garmin",
+  m: "Imperial Treausre Fine Shanghai Cuisine",
+  n: "Kanshoku Ramen Bar",
+  o: "Lacoste",
+  p: "Louis Vuitton",
+  q: "Mango",
+  r: "Omega",
+  s: "Rolex",
+  t: "Seiko",
+  u: "Superdry",
+  v: "Tailored Fashions",
+  w: "Tampopo Ramen",
+  x: "Typo",
+  y: "Yanmi Yogurt",
+  0: "No selection",
+};
 
 var storeToMall = {
-  'a':[1,],
-  'b':[1,2],
-  'c':[3,],
-  'd':[1,3],
-  'e':[1,],
-  'f':[1,3],
-  'g':[1,2],
-  'h':[2,],
-  'i':[2,],
-  'j':[3,],
-  'k':[2,],
-  'l':[2,],
-  'm':[3,],
-  'n':[1,],
-  'o':[2,],
-  'p':[1,3],
-  'q':[1,2],
-  'r':[1,],
-  's':[1,],
-  't':[3,],
-  'u':[3,],
-  'v':[3,],
-  'w':[3,],
-  'x':[2,],
-  'y':[2,],
-}
+  a: [1],
+  b: [1, 2],
+  c: [3],
+  d: [1, 3],
+  e: [1],
+  f: [1, 3],
+  g: [1, 2],
+  h: [2],
+  i: [2],
+  j: [3],
+  k: [2],
+  l: [2],
+  m: [3],
+  n: [1],
+  o: [2],
+  p: [1, 3],
+  q: [1, 2],
+  r: [1],
+  s: [1],
+  t: [3],
+  u: [3],
+  v: [3],
+  w: [3],
+  x: [2],
+  y: [2],
+};
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const history = [
-    {
-      date: "12 June 2021",
-      cases: Math.floor(Math.random() * 5) + 1,
-    },
-    {
-      date: "10 June 2021",
-      cases: Math.floor(Math.random() * 10) + 1,
-    },
-    {
-      date: "5 June 2021",
-      cases: Math.floor(Math.random() * 20) + 1,
-    },
+  {
+    date: "12 June 2021",
+    cases: Math.floor(Math.random() * 5) + 1,
+  },
+  {
+    date: "10 June 2021",
+    cases: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    date: "5 June 2021",
+    cases: Math.floor(Math.random() * 20) + 1,
+  },
 ];
 
 const history2 = [
@@ -107,7 +111,7 @@ const history2 = [
   {
     date: "28 May 2021",
     cases: Math.floor(Math.random() * 20) + 1,
-  }
+  },
 ];
 
 const history3 = [
@@ -134,26 +138,25 @@ const Table = () => {
         <DataTable.Title>Date</DataTable.Title>
         <DataTable.Title numeric>Cases</DataTable.Title>
       </DataTable.Header>
-      
+
       {dict[mallName].map((hist) => (
         <DataTable.Row>
           <DataTable.Cell>{hist.date}</DataTable.Cell>
           <DataTable.Cell numeric>{hist.cases}</DataTable.Cell>
         </DataTable.Row>
       ))}
-      
     </DataTable>
   );
-}
-
+};
 
 function TabA() {
   var count = Math.floor(Math.random() * 1000) + 1;
-  var level = (count > 500)
+  var level = count > 500;
   const [selectedValue, setSelectedValue] = useState("java");
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Estimated Count: 
+      <Text style={styles.text}>
+        Estimated Count:
         <Text style={level ? styles.danger : styles.normal}> {count}</Text>
       </Text>
       <chooser />
@@ -165,7 +168,8 @@ function TabB() {
   const { mallName, storeName } = current.params;
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Past Cases for 
+      <Text style={styles.text}>
+        Past Cases for
         <Text style={styles.malltext}> {mallDict[mallName]}</Text>
       </Text>
       <Table />
@@ -182,38 +186,36 @@ const Alt_Table = () => {
   var keys = storeToMall[storeName];
   if (storeName === undefined || keys === undefined) {
     places.push("Please select a store.");
-  }
-  else {
-   for (var i = 0; i < keys.length; i++) {
-    if (mallDict[keys[i]] !==  mallDict[mallName]) {
-      places.push(mallDict[keys[i]]);
+  } else {
+    for (var i = 0; i < keys.length; i++) {
+      if (mallDict[keys[i]] !== mallDict[mallName]) {
+        places.push(mallDict[keys[i]]);
+      }
     }
-   }
   }
 
   if (places.length === 0) {
-    places.push("There are no other alternatives. Please select a different outlet");
+    places.push(
+      "There are no other alternatives. Please select a different outlet"
+    );
   }
-  return (    
-  <DataTable>
-    <DataTable.Header>
-      <DataTable.Title>Places</DataTable.Title>
-    </DataTable.Header>
+  return (
+    <DataTable>
+      <DataTable.Header>
+        <DataTable.Title>Places</DataTable.Title>
+      </DataTable.Header>
 
-    {places.map((place) => (
+      {places.map((place) => (
         <DataTable.Row>
           <DataTable.Cell>{place}</DataTable.Cell>
         </DataTable.Row>
       ))}
+    </DataTable>
+  );
+};
 
-  </DataTable>)
-}
-
-function TabC({route, navigator}) {
-
+function TabC({ route, navigator }) {
   const { mallName, storeName } = current.params;
-  
-  
 
   return (
     <View style={styles.container}>
@@ -224,47 +226,47 @@ function TabC({route, navigator}) {
   );
 }
 
-
-function ReactNavigationBottomTabs( {route, navigator} ) {
-
-current = route;
-/*  const { name } = route.params;
+function ReactNavigationBottomTabs({ route, navigator }) {
+  current = route;
+  /*  const { name } = route.params;
   return (
     <View>
       <Text>{JSON.stringify(name)}</Text>
     </View>
   )*/
-  
+
   return (
-      <Tab.Navigator>
-        <Tab.Screen
-          name='Headcount'
-          component={TabA}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name='person' color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='History'
-          component={TabB}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name='history' color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Alternatives'
-          component={TabC}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name='list' color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{ activeTintColor: "green", inactiveTintColor: "grey" }}
+    >
+      <Tab.Screen
+        name="Headcount"
+        component={TabA}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="person" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="History"
+        component={TabB}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="history" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Alternatives"
+        component={TabC}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="list" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
@@ -282,29 +284,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fac8f8",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     fontSize: 40,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   malltext: {
     fontSize: 40,
-    fontWeight: 'bold',
-    color: 'blue'
+    fontWeight: "bold",
+    color: "blue",
   },
   danger: {
-    color: 'red'
+    color: "red",
   },
   normal: {
-    color: 'green'
+    color: "green",
   },
   head: {
     height: 40,
-    backgroundColor: 'grey'
+    backgroundColor: "grey",
   },
   tabletext: {
-    margin: 6
-  }
+    margin: 6,
+  },
 });
--- project1/Firstscreen.js
+++ project1/Firstscreen.js
@@ -1,107 +1,97 @@
 import React, { useState } from "react";
-import { Button, View, StyleSheet, Switch, Text } from "react-native";
+import { Button, ImageBackground, View, StyleSheet, Text } from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { propTypes } from "react-bootstrap/esm/Image";
 import ionMenu from "./ionShops.js";
 import wismaMenu from "./wismaShops.js";
 import NgeeAnnMenu from "./NgeeAnnShops.js";
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from "@react-native-picker/picker";
 
 export default function chooser({ navigation }) {
   const [locationId, setSelectedId] = useState("1");
-  const [locationOutlet, setSelectedOutlet] = useState('0');
+  const [locationOutlet, setSelectedOutlet] = useState("0");
+  const image = {
+    uri: "https://mcdn.wallpapersafari.com/medium/95/98/3vRqDI.jpg",
+  };
 
   return (
-    <View>
-      <View style={styles.text_box}>
-      <Text style={styles.textActual}>Please select shopping center</Text>
-      </View>
-      <View style={styles.dropdownBox}>
-        
-        
-        <Picker
-          selectedValue={locationId}
-          style={{ height: 50, width: 150 }}
-          onValueChange={(itemValue) => {
-            setSelectedOutlet("0");
-            setSelectedId(itemValue);}
-          }
-        >
-          <Picker.Item label="Ion Orchard" value="1" />
-          <Picker.Item label="Wisma Atria" value="2" />
-          <Picker.Item label="Ngee Ann City" value="3" />
-        </Picker>
-      </View>
-      <View style={styles.text_box}>
-        <Text style={styles.textActual}>Which store are you visiting?</Text>
-      </View>
-      <View style={styles.dropdownBox}>
-        {(() => {
-          
-          switch (locationId) {
-            case "1":
-              return (
-                ionMenu(locationOutlet, setSelectedOutlet)
-              );
-
-            case "2":
-              return (
-                wismaMenu(locationOutlet, setSelectedOutlet)
-              );
-
-            case "3":
-              return (
-                NgeeAnnMenu(locationOutlet, setSelectedOutlet)
-              );
-          }
-        })()}
-      </View>
-      <View style={styles.button_format}>
-      <Button
-          color="orange"
-          title="View Recommendations"
-          onPress={() => navigation.navigate('Profile', { mallName: locationId, storeName: locationOutlet },
-          )} //additional of a button to navigate elsewhere
-        />
+    <View style={styles.container}>
+      <ImageBackground source={image} style={styles.image}>
+        <View style={styles.text_box}>
+          <Text style={styles.textActual}>Please select a shopping center</Text>
         </View>
+        <View style={styles.dropdownBox}>
+          <Picker
+            selectedValue={locationId}
+            style={{ height: 50, width: 150 }}
+            onValueChange={(itemValue) => {
+              setSelectedOutlet("0");
+              setSelectedId(itemValue);
+            }}
+          >
+            <Picker.Item label="Ion Orchard" value="1" />
+            <Picker.Item label="Wisma Atria" value="2" />
+            <Picker.Item label="Ngee Ann City" value="3" />
+          </Picker>
+        </View>
+        <View style={styles.text_box}>
+          <Text style={styles.textActual}>Which store are you visiting?</Text>
+        </View>
+        <View style={styles.dropdownBox}>
+          {(() => {
+            switch (locationId) {
+              case "1":
+                return ionMenu(locationOutlet, setSelectedOutlet);
+
+              case "2":
+                return wismaMenu(locationOutlet, setSelectedOutlet);
+
+              case "3":
+                return NgeeAnnMenu(locationOutlet, setSelectedOutlet);
+            }
+          })()}
+        </View>
+        <View style={styles.button_format}>
+          <Button
+            color="orange"
+            title="View Recommendations"
+            onPress={() =>
+              navigation.navigate("Profile", {
+                mallName: locationId,
+                storeName: locationOutlet,
+              })
+            } //additional of a button to navigate elsewhere
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  dropdownBox: {
-    flex: 2,
-    margin: "10%",
-    alignItems: "center",
+  container: {
+    alignItems: "fill",
     justifyContent: "center",
   },
-  profileBox: {
-    flex: 2,
-    paddingTop: "50%",
+  dropdownBox: {
     alignItems: "center",
-    justifyContent: "center",
+    padding: "1%",
   },
   button_format: {
-    flex: 1,
-    margin: "10%",
     alignItems: "center",
-    justifyContent: "center"
+    padding: "3%",
   },
   text_box: {
-    flex: 1,
     fontWeight: "bold",
     fontSize: 40,
-    margin: "10%",
     alignItems: "center",
-    justifyContent: "center"
-
+    padding: "1%",
   },
   textActual: {
-    
     fontWeight: "bold",
+    color: "white",
     fontSize: 40,
-    
-
-  }
+    padding: "1%",
+  },
 });
